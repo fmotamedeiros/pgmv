e615bd58c3cf492e4ea0636cf3a8f1e2bf5e17b8
Flávio Medeiros <flaviomotamedeiros@gmail.com>
2023-08-24 09:31:11 -0300

    Exemplo de navegação com pilha.

---
+++ ReactNative/stack/App.js
@@ -0,0 +1,62 @@
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+function Login() {
+
+  const navigator = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Text>Login!</Text>
+      <Button title='Entrar' onPress={() => navigator.replace('Home')}></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function Home() {
+  const navigator = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Text>Home!</Text>
+      <Button title='Sair' onPress={() => navigator.replace('Login')}></Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={Home} options={{
+          title: 'My home',
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
